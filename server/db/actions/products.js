ae34451b09d74b8a0472a8b2ed0f5f13186f5c90
jkrgS <siskogeorge@gmail.com>
2021-07-05 19:05:07 +0300

    added routers, new db action, getting products from query with pagination/sorting

--- server/db/actions/products.js
+++ server/db/actions/products.js
@@ -2,12 +2,13 @@ const ProductsSchema = require('../models/productsSchema');
 const { IProducts } = require('../../interfaces/products');
 
 // check if products exists
-const checkProducts = async () => {
+const countProducts = async () => {
   return await ProductsSchema.countDocuments({}, (error, count) =>
     !error ? count : error
   );
 };
 
+// insert products
 const insertProducts = async (products = IProducts) => {
   try {
     const productsInsert = products.map((product) => ({
@@ -19,4 +20,24 @@ const insertProducts = async (products = IProducts) => {
     console.error(error);
   }
 };
-module.exports = { checkProducts, insertProducts };
+
+const getProducts = async (args) => {
+  try {
+    const { from = 1, size = 10, sorting = { title: 1 } } = args; // deconstruct arguments
+    let productsCount; // constant for keeping the count of documents in db
+
+    await countProducts().then((count) => (productsCount = count)); // get the count of documents from existing action
+    // get products sorted and paginated
+    const products = await ProductsSchema.find({})
+      .sort(sorting)
+      .skip((+from - 1) * +size)
+      .limit(+size)
+      .exec();
+
+    return { products, count: productsCount };
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { countProducts, insertProducts, getProducts };
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router(),
+  api = require('./api');
+
+router.use('/api', api);
+
+router.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function (errors, key) {
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {}),
+    });
+  }
+
+  return next(err);
+});
+
+module.exports = router;
