de017eeb114004d8c7a8d43fc02529370fe56b8a
jkrgS <siskogeorge@gmail.com>
2021-07-05 16:17:03 +0300

    csv file read data(local file within assets for dev purposes)

---
+++ server/app.js
@@ -0,0 +1,3 @@
+const { readCsv } = require('./services/csvReader');
+
+readCsv();
---
+++ server/models/_csv.js
@@ -0,0 +1,21 @@
+const _csv = [
+  'id',
+  'title',
+  'description',
+  'link',
+  'image_link',
+  'availability',
+  'price',
+  'brand',
+  'gtin',
+  'mpn',
+  'condition',
+  'shipping',
+  'identifier_exists',
+  'google_product_category',
+  'adult',
+  'gender',
+  'color',
+];
+
+module.exports = { _csv };
